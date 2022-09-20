a8bc1ac788973e72a946dd14a1b3a75116a3ba26
Imran <imranmansuri038@gmail.com>
2022-09-20 20:27:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Overview from "./pages/Overview";
+import { Reports, ReportsOne, ReportsTwo, ReportsThree } from "./pages/Reports";
+import Team from "./pages/Team";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/overview" exact element={<Overview />} />
+        <Route path="/reports" exact element={<Reports />} />
+        <Route path="/reports/reports1" exact element={<ReportsOne />} />
+        <Route path="/reports/reports2" exact element={<ReportsTwo />} />
+        <Route path="/reports/reports3" exact element={<ReportsThree />} />
+        <Route path="/team" exact element={<Team />} />
+      </Routes>
+    </Router>
   );
 }
 
